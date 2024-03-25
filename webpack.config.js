e7ad0ecb849308c5c58adfa2f71a15b931dfba5c
Lincoln Bryant <lincolnb@uchicago.edu>
2024-03-25 16:40:14 -0500

    fix webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,75 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'production', 
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
+module.exports = (env, argv) => {
+  return {
+      mode: 'production',
+      entry: './src/index.js',
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+        publicPath: '/',
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
+      module: {
+        rules: [
           {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+              },
             },
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'static',
+                },
+              },
+            ],
           },
         ],
       },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'login.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public/favicon.ico', to: 'favicon.ico' },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: 'index.html',
+          inject: false,
+        }),
+        new CopyWebpackPlugin({
+          patterns: [
+            { from: 'public/favicon.ico', to: 'favicon.ico' },
+          ],
+        }),
       ],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 3000,
-  },
-  performance: {
-    hints: false, // or 'warning' or 'error'
-    maxEntrypointSize: 512000, // 500KB
-    maxAssetSize: 512000, // 500KB
-  },
+      devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3000,
+        open: {
+          app: {
+            name: 'chrome',
+            arguments: ['--incognito', '--new-window'],
+          },
+          target: ['/index.html'],
+        },
+      },
+      performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+      },
+   };
 };
